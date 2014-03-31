f0736a54667c9084f2a18b1067c0ec2fc224f648
jcwrequests@comcast.net <jcwrequests@comcast.net>
2014-03-31 19:30:42 -0400

    Frist Interation of TinyQueue

---
+++ Biggy.TinyFS/TinyQueue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biggy.TinyFS
+{
+    public class TinyQueue<T> 
+    {
+        TinyList<T> store;
+        object queueLock;
+        public TinyQueue(TinyList<T> store)
+        {
+            if (store == null) throw new ArgumentNullException("store");
+            queueLock = new object();
+            this.store = store;
+        }
+        public void EnQueue(T item){
+            lock (queueLock)
+            {
+                store.Add(item);
+            }
+        }
+        public T DeQueue() {
+            lock (queueLock)
+            {
+                var item = store.FirstOrDefault();
+                if (item != null)
+                {
+                    store.Remove(item);
+                    return item;
+                } 
+                return default(T);
+            }
+        }
+        public T Peek() {
+            lock (queueLock)
+            {
+                var item = store.FirstOrDefault();
+                if (item != null)
+                {
+                    return item;
+                }
+                return default(T);
+            }
+        }
+        public int MessageCount() { return store.Count; }
+
+        public IEnumerator<T> GetEnumerator() {
+            lock (queueLock)
+            {
+                return store.GetEnumerator();
+            }
+        }
+
+    }
+}
