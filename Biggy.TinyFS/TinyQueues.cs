877f458b527d8fe07b179f3f65706b28855744e8
jcwrequests@comcast.net <jcwrequests@comcast.net>
2014-03-31 19:35:21 -0400

    Started TinyQueues Embeded Server

---
+++ Biggy.TinyFS/TinyQueues.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biggy.TinyFS
+{
+    public class TinyQueues
+    {
+        TinyDB db;
+
+        public TinyQueues(TinyDB db)
+        {
+            if (db == null) throw new ArgumentNullException("db");
+            this.db = db;
+        }
+    }
+}
